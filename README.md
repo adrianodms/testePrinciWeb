f6f0a295462cd182d1072c5de8e02aad916de3e2
Adriano Magalhães <adrianodmagalhaes@gmail.com>
2016-09-11 20:40:18 -0300

    configuração de build para produção. Atualização do readme.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# testePrinciWeb
+# Teste PrinciWeb
+Este é um teste feito por Adriano Magalhães.
+
+Para conhecer poder vizualizar o resultado, você pode ver através deste [link](http://placeholder.teste.com.br)
+ ou baixando o diretório.
+
+## Rodando o Projeto
+
+Para rodar o projeto pela primeira vez, digite no seu terminal: **npm install**.
+
+Após a instalação é só digitar **gulp**.
+
+
+## Tasks do Gulp
+
+_**serve**_ (essa é a default): faz o build do less e roda um servidor local para testes. Essa task atualiza a pagina toda vez que há alterações dos arquivos e js e html, e também injeta css em tempo real quando há alterações do less.
+
+_**build**_: faz uma cópia dos arquivos de desenvolvimento(que estão na pasta src) para a pasta dist, compila o less, minifica e concatena arquivos css/js.
+
+_**clean-after-build**_: limpa arquivos desnecessários da build, para poder subir em produção. (infelizmente não consegui deixar como depêndencia da task 'build')
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,25 @@
 var gulp = require('gulp');
+
+var autoprefixer = require('gulp-autoprefixer');
 var clean = require('gulp-clean');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
 var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
-var rev = require('gulp-rev');
 
 
 gulp.task('clean', function () {
-    gulp.src('dist').pipe(clean());
+    gulp.src('dist')
+        .pipe(clean());
 });
 
-gulp.task('copy', function () {
-    gulp.src('src/**/*')
-        .pipe(gulp.dest('dist'));
-})
-gulp.task('clean-build', function(){
-    gulp.src('dist/img/sprite', 'dist/css/*.less')
-        .pipe(clean());
-})
+gulp.task('copy', ['clean'], function(){
 
-gulp.task('min-build', function(){
-    gulp.src('src/index.html')
-    .pipe(usemin({
-      css: [  minifyCss(), 'concat', rev()],      
-      js: [ uglify(), rev() ]     
-    }))
-    .pipe(gulp.dest('build/'));
-})
+	return gulp.src('src/**/*')
+		.pipe(gulp.dest('dist'));
 
-gulp.task('build', ['clean', 'copy', 'min-build','clean-build']);
+});
 
 gulp.task('less', function () {
     return gulp.src('src/css/main.less')
@@ -40,7 +29,6 @@ gulp.task('less', function () {
 });
 
 gulp.task('serve', ['less'], function () {
-
     browserSync.init({
         server: "./src"
     });
@@ -50,5 +38,32 @@ gulp.task('serve', ['less'], function () {
     gulp.watch("src/**/*.js").on('change', browserSync.reload);
 });
 
+gulp.task('usemin', function(){
+	gulp.src('dist/**/*.html')
+	.pipe(usemin({
+		'js':[uglify],
+        'css':[autoprefixer, minifyCss]	
+	}))
+	.pipe(gulp.dest('dist'))
+});
+
+gulp.task('build', ['copy'], function(){
+    gulp.start('usemin');
+    console.log('A versão de produção está pronta. Mas é recomenado rodar a task "clean-after-build" para remover os arquivos desnecessários.')  
+});
+
+gulp.task('clean-build-sprites', function(){    
+    gulp.src('dist/img/sprite').pipe(clean()); 
+});
+
+gulp.task('clean-build-css', function(){    
+    gulp.src(['dist/css/**.less', 'dist/css/vendor', 'dist/css/main.css']).pipe(clean());     
+})
+
+gulp.task('clean-build-js', function(){    
+    gulp.src(['dist/js/main.js', 'dist/js/vendor']).pipe(clean()); 
+})
+
+gulp.task('clean-after-build', ['clean-build-sprites', 'clean-build-js', 'clean-build-css']);
 
 gulp.task('default', ['serve']);
