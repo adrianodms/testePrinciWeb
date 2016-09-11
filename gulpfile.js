c6564f28bec23b2ad867a24b9beb2471206f48cd
Adriano Magalhães <adrianodmagalhaes@gmail.com>
2016-09-11 18:11:20 -0300

    implementado funcionalidades do header(busca e menu mobile). Ajustes em todo site para funcionalidades mobile. Implementado interação do formulário de contato.

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,44 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
 
 
-gulp.task('clean', function(){
-	return gulp.src('dist').pipe(clean());
+gulp.task('clean', function () {
+    gulp.src('dist').pipe(clean());
 });
 
-gulp.task('build', ['clean'], function(){
-	return gulp.src('src/**/*')
-		.pipe(gulp.dest('dist'));
-});
+gulp.task('copy', function () {
+    gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
+})
+gulp.task('clean-build', function(){
+    gulp.src('dist/img/sprite', 'dist/css/*.less')
+        .pipe(clean());
+})
 
-gulp.task('less', function(){
+gulp.task('min-build', function(){
+    gulp.src('src/index.html')
+    .pipe(usemin({
+      css: [  minifyCss(), 'concat', rev()],      
+      js: [ uglify(), rev() ]     
+    }))
+    .pipe(gulp.dest('build/'));
+})
+
+gulp.task('build', ['clean', 'copy', 'min-build','clean-build']);
+
+gulp.task('less', function () {
     return gulp.src('src/css/main.less')
         .pipe(less())
         .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.stream({match: '**/*.css'}));
+        .pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
-gulp.task('serve', ['less'], function() {
+gulp.task('serve', ['less'], function () {
 
     browserSync.init({
         server: "./src"
@@ -28,7 +47,7 @@ gulp.task('serve', ['less'], function() {
 
     gulp.watch("src/css/*.less", ['less']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
-    gulp.watch("src/*.js").on('change', browserSync.reload);
+    gulp.watch("src/**/*.js").on('change', browserSync.reload);
 });
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,106 @@
-function initSiteMap(){
-    $('#siteMapToggle').on('click', function(e){
+function initSiteMap() {
+    $('#siteMapToggle').on('click', function (e) {
         e.preventDefault();
         $('#siteMap').toggleClass('active');
     });
 }
 
-function initSearch(){
-    $('#searchToggle').on('click', function(e){
+function initSearch() {
+    $('#searchToggle').on('click', function (e) {
         e.preventDefault();
         $('#search').toggleClass('active');
-        if($('#search').is('.active')){
-            $('#searchInput').focus();  
-        } 
+        if ($('#search').is('.active')) {
+            $('#searchInput').focus();
+        }
     });
 }
 
-function carrousel(){
+function carrousel() {
     $('#slider').slick({
-       autoplay: true,
-       autoplaySpeed: 8000,
-       arrows:false
+        autoplay: true,
+        autoplaySpeed: 8000,
+        arrows: false
     });
 }
 
-$(function(){
+function initToggleMenu() {
+    $('#toggleMenu').on('click', function (e) {
+        e.preventDefault();
+        $('#navigation').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+    $('#navigation').on('click', function () {
+        $('#toggleMenu').removeClass('active');
+        $(this).removeClass('active');
+    });
+}
+
+function contactForm($form, fields) {
+    var self = this;
+
+    $form.on('submit', function (e) {
+        e.preventDefault();
+        self.submit();
+    });
+
+    self.fields = fields;
+
+    self.validate = function () {
+        var valid = true;
+        for (var i = 0; i < self.fields.length; i++) {
+            if (!self.fields[i].val().trim().length) {
+                self.setInvalidField(self.fields[i])
+                valid = false;
+            }
+            if (self.fields[i].is('[type=email]')) {
+                var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (!re.test(self.fields[i].val())) {
+                    self.setInvalidField(self.fields[i]);
+                    valid = false;
+                };
+            }
+        }
+        return valid;
+    }
+
+    self.setInvalidField = function ($field) {
+        $field.closest('.form-group').addClass('has-error');
+        $field.closest('.form-group').off('click.validate').on('click.validate', function () {
+            $(this).removeClass('has-error');
+        });
+    }
+
+    self.submit = function () {        
+        if (validate()) {
+            // aqui simulei um post com o setTimeout apenas para visualização do teste
+            self.changeState('sending');
+            window.setTimeout(function () {  
+                self.changeState('sent');
+                window.setTimeout(function(){
+                    self.changeState();
+                    $form.trigger('reset');
+                },7000)
+            }, 3000);
+        } 
+    }
+    
+    self.changeState = function (state) {
+        $form.removeClass(function (index, css) {
+            return (css.match (/(^|\s)state-\S+/g) || []).join(' ');
+        });
+        if(state){
+            $form.addClass('state-'+state);
+        }        
+    }
+}
+
+$(function () {
     initSiteMap();
     initSearch();
-    carrousel();
+    initToggleMenu();    
+    carrousel();    
+    contactForm(
+        $('#contactForm'),
+        [$('#nome'), $('#email'), $('#telefone'), $('#mensagem')]
+    );
 })
